4d1adc2868404a9e79fb4bd67ddcc392b74311cf
EnriqueRojas <enriquer803@gmail.com>
2022-08-30 12:40:47 -0500

    Codigo 2 (ids)

---
+++ 02Codigo_ids.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  printf("Real user ID: %d\n", getuid());
+  printf("Effective user ID: %d\n", geteuid());
+  printf("Real group ID: %d\n", getgid());
+  printf("Effective group ID: %d\n", getegid());
+  return 0;
+}
