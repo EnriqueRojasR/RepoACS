64e4086728fc9be6ee4e281e9b4fecd1f66a6172
EnriqueRojas <enriquer803@gmail.com>
2022-09-20 23:54:02 -0500

    Codigo 18

--- 18Codigo_execve1.c
+++ 18Codigo_execve1.c
@@ -7,6 +7,7 @@
 // para leer la variable de entorno USER
 int main(void)
 {
+  printf("Rojas Ruiz Luis Enrique \n");
   puts("En el proceso inicial, antes de llamar a execve\n");
   // como es execve entonces es un vector/arreglo
   // y ademas puede leer variables de entorno que vienen en un arreglo
@@ -17,12 +18,12 @@ int main(void)
   // En C el nombre del comando/ejecutable es el argumento 0
   // por eso se repite
   // con NULL se indica que se llego al final del vector/arreglo
-  char * args[]={"./getenv_user2", NULL};
+  char * args[]={"./14Codigo_getenv_user2", NULL};
   char *variables_entorno[] = { "PATH=/bin:/usr/bin", "USER=joe", "MI_VARIABLE=acs", NULL };
   printf( "el valor de la variable de entorno USER es: %s\n", getenv("USER") );
   printf( "el valor de la variable de entorno MI_VARIABLE es: %s\n", getenv("MI_VARIABLE") );
-  printf( "ahora ejecutando ./getenv_user2, los valores de las variables de entorno USER y MI_VARIABLE son: \n" );
-  if( execve("./getenv_user2", args, variables_entorno) == -1)
+  printf( "ahora ejecutando ./14Codigo_getenv_user2, los valores de las variables de entorno USER y MI_VARIABLE son: \n" );
+  if( execve("./14Codigo_getenv_user2", args, variables_entorno) == -1)
   {
     puts("execve genero error y devolvio -1");
     perror("execve");
