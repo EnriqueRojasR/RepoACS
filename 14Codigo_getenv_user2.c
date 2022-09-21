970c5cba21e7625e1d1c0eca2ab5f54f788b5018
EnriqueRojas <enriquer803@gmail.com>
2022-09-20 23:33:16 -0500

    Codigo 14 printf

--- 14Codigo_getenv_user2.c
+++ 14Codigo_getenv_user2.c
@@ -4,6 +4,7 @@
 
 int main(void)
 {
+  printf("Rojas Ruiz Luis Enrique \n");
   printf( "el valor de la variable de entorno USER es: %s\n", getenv("USER") );
   printf( "el valor de la variable de entorno MI_VARIABLE es: %s\n", getenv("MI_VARIABLE") );
   return 0;
