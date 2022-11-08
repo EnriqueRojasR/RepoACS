7061fffd0af6cab8c9efb921362535da096eec4e
EnriqueRojas <enriquer803@gmail.com>
2022-11-08 07:19:16 -0600

    Server

---
+++ 31Codigo_serverUnix02.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+int main()
+{
+ // este es el descriptor de archivo o file descriptor
+ // se le asignara el valor devuelto por la funcion socket
+ int socket_fd;
+
+ // creamos el socket
+ // la funcion socket devuelve un int que es el file descriptor
+ printf("socket (AF_UNIX, SOCK_STREAM, 0)\n");
+ socket_fd = socket (AF_UNIX, SOCK_STREAM, 0);
+ printf("socket_fd=%d\n",socket_fd);
+
+ // una variable de tipo struct sockaddr_un
+ // que sera nuestra estructura server socket
+ struct sockaddr_un servidor;
+
+ // un apuntador a la variable anterior
+ struct sockaddr* pservidor = (struct sockaddr*)&servidor;
+
+ // este int almacenara el size de la estructura
+ // sockaddr_un que es nuestra estructura server socket
+ int size_servidor = sizeof(servidor);
+
+ // el archivo con el que se nombrara al socket
+ // el archivo no debe existir y se creara con size 0
+ const char* const socket_name = "socket";
+
+ // inicializamos el atributo sun_family de la struct
+ servidor.sun_family = AF_UNIX;
+
+ // inicializamos el atributo sun_path con el nombre del archivo
+ strcpy( servidor.sun_path, socket_name );
+
+ // Ligamos el socket con la estructura servidor y como esta
+ // tiene el nombre del archivo, entonces el socket se hace
+ // un socket nombrado
+ printf("bind(socket_fd,pservidor,size_servidor)\n");
+ bind(socket_fd,pservidor,size_servidor);
+ printf("socket_fd=%d\n",socket_fd);
+
+ // El servidor se pone en modo de escucha para conexiones cliente
+ printf("listen (socket_fd, 5)\n");
+ listen (socket_fd, 5);
+
+ // Las siguientes 3 sentencias crean una estructura sockadd_un
+ // Obtiene su apuntador y su size o longitud
+ // Esta estructura se "hablara" con el socket del lado del cliente
+ // en cuanto sea aceptada la conexion del cliente
+ struct sockaddr_un cliente;
+ struct sockaddr* pcliente = (struct sockaddr*)&cliente;
+ int size_cliente = sizeof(cliente);
+
+ printf("Antes de la funcion accept()\n");
+ // Esta funcion bloquea hasta que llegue una peticion de conexion de un cliente
+ int client_socket_fd = accept (socket_fd, (struct sockaddr *)pcliente, &size_cliente);
+ printf("Se acepto la conexion de un cliente\n");
+ printf("socket_fd=%d\n",socket_fd);
+ printf("client_socket_fd=%d\n",client_socket_fd);
+
+ printf("close(socket_fd)\n");
+ close( socket_fd );
+ printf("close(client_socket_fd)\n");
+ close( client_socket_fd );
+ printf("unlink( socket_name )\n");
+ unlink( socket_name );
+ printf("socket_fd=%d\n",socket_fd);
+ printf("client_socket_fd=%d\n",client_socket_fd);
+ return (0);
+}
---
+++ 32Codigo_clientUnix02.c
@@ -0,0 +1,52 @@
+/************************************************************/
+/**************                         cliente.c           **********************/
+/************************************************************/
+/*********                   proceso cliente con sockets AF_UNIX   **********/
+/************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <sys/un.h>           /*  para sockets UNIX  */
+
+#define PROTOCOLO_DEFECTO 0
+
+/****************************************************/
+
+int main()
+{
+ int dfClient, longitud, resultado;
+ struct sockaddr_un dirUNIXServer;
+ struct sockaddr* puntSockServer;
+
+ puntSockServer = ( struct sockaddr* ) &dirUNIXServer;
+ longitud = sizeof ( dirUNIXServer );
+
+ printf("socket (AF_UNIX, SOCK_STREAM, 0)\n");
+ dfClient = socket ( AF_UNIX, SOCK_STREAM, PROTOCOLO_DEFECTO );
+                 /* se crea un socket UNIX, bidireccional */
+ printf("dfClient=%d\n",dfClient);
+
+ dirUNIXServer.sun_family = AF_UNIX;    /* tipo de dominio server */
+ strcpy ( dirUNIXServer.sun_path, "socket" );   /* nombre server */
+
+ do
+  {
+   printf("Antes de la funcion connect()\n");
+   resultado = connect ( dfClient, puntSockServer, longitud );
+   if ( resultado == -1 )
+     sleep (1);   /* reintento */
+ }
+ while ( resultado == -1 );
+   printf("Despues de la funcion connect()\n");
+
+ printf("close(dfClient)\n");
+ close ( dfClient );      /* cierra el socket */
+ return (0);     /* buen fin */
+
+}
+
+/************* fin de cliente.c ****************************/
